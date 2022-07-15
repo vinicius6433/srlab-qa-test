62303378f8358aca797d25c758fac571d7a5c119
Vinicius Moreira <79025625+vinicius6433@users.noreply.github.com>
2022-07-15 15:19:27 -0300

    Atualização de projetos

--- src/test/java/br/com/southrocklab/resources/CardResourceTest.java
+++ src/test/java/br/com/southrocklab/resources/CardResourceTest.java
@@ -1,35 +1,118 @@
 package br.com.southrocklab.resources;
 
-import br.com.southrocklab.ApplicationTests;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import br.com.southrocklab.ApplicationTests;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 public class CardResourceTest extends ApplicationTests {
+	String card_id;
+
+	@BeforeClass
+	public static void urlbase() {
+		RestAssured.port = 9090;
+	}
+
+	 @Test
+	public void deve_salvar_novo_card_no_sistema() {
+		Response response = given().contentType("application/json").body(
+				"{ \"brand\": \"MASTER\", \"customerId\": 1, \"cvc\": \"901\", \"expirationMoth\": 9, \"expirationYear\": 2025, \"holderName\": \"Vinicius\", \"number\": \"5165734389002316\" }")
+				.when().post("/card");
+
+		response.then().statusCode(200).log().all();
+
+	}
 
     @Test
-    public void deve_salvar_novo_card_no_sistema() {
-    }
+	public void deve_retornar_status_400_quando_salvar_card_com_cvv_maior_que_999() {
+		Response response = given().contentType("application/json").body(
+				"{ \"brand\": \"MASTER\", \"customerId\": 0, \"cvc\": \"10000\", \"expirationMoth\": 9, \"expirationYear\": 2025, \"holderName\": \"Vinicius\", \"number\": \"5165734389002316\" }")
+				.when().post("/card");
+
+		response.then().log().all().body("cvc", containsString("cvc must be between 001 to 999")).statusCode(400);
+	}
 
     @Test
-    public void deve_retornar_status_400_quando_salvar_card_com_cvv_maior_que_999() {
-    }
+	public void deve_retornar_status_400_quando_salvar_card_com_expiration_month_maior_que_12() {
+		Response response = given().contentType("application/json").body(
+				"{ \"brand\": \"MASTER\", \"customerId\": 0, \"cvc\": \"709\", \"expirationMoth\": 13, \"expirationYear\": 2025, \"holderName\": \"Vinicius\", \"number\": \"5165734389002316\" }")
+				.when().post("/card");
+
+		response.then().log().all().body("expirationMoth", containsString("Expiration month must be less then 12"))
+				.statusCode(400);
+	}
 
     @Test
-    public void deve_retornar_status_400_quando_salvar_card_com_expiration_month_maior_que_12() {
-    }
+	public void deve_retornar_status_400_quando_salvar_card_com_expiration_year_menor_que_2022() {
+		Response response = given().contentType("application/json").body(
+				"{ \"brand\": \"MASTER\", \"customerId\": 0, \"cvc\": \"709\", \"expirationMoth\": 11, \"expirationYear\": 2021, \"holderName\": \"Vinicius\", \"number\": \"5165734389002316\" }")
+				.when().post("/card");
+
+		response.then().log().all()
+				.body("expirationYear", containsString("Expiration year must be greater than the current one"))
+				.statusCode(400);
+	}
 
     @Test
-    public void deve_retornar_status_400_quando_salvar_card_com_expiration_year_menor_que_2022() {
-    }
+	public void deve_retornar_status_400_quando_salvar_card_com_number_de_15_digitos() {
+		Response response = given().contentType("application/json").body(
+				"{ \"brand\": \"MASTER\", \"customerId\": 0, \"cvc\": \"709\", \"expirationMoth\": 11, \"expirationYear\": 2025, \"holderName\": \"Vinicius\", \"number\": \"516573438900231\" }")
+				.when().post("/card");
 
-    @Test
-    public void deve_retornar_status_400_quando_salvar_card_com_number_de_15_digitos() {
-    }
+		response.then().log().all().body("number", containsString("Number must have a 16 numbers")).statusCode(400);
+	}
 
-    @Test
-    public void deve_deletar_um_card_salvo_no_sistema() {
-    }
+	@Test
+	public void deve_deletar_um_card_salvo_no_sistema() {
+		criarCard();
+		
+		Response response = 
+				given()
+					.contentType("application/json")
+					.pathParam("id", card_id)
+				.when()
+					.delete("/card/{id}");
 
-    @Test
-    public void deve_retornar_status_404_ao_deletar_um_card_com_id_nao_salvo_no_sistema() {
-    }
+			response
+				.then()
+					.statusCode(204)
+					.log().all();
+	}
+
+	@Test
+	public void deve_retornar_status_404_ao_deletar_um_card_com_id_nao_salvo_no_sistema() {
+		Response response = 
+			given()
+				.contentType("application/json")
+			.when()
+				.delete("/card/10");
+
+		response
+			.then()
+				.statusCode(404)
+				.body("error", containsString("Not Found"))
+				.log().all();
+	}
+
+	private void criarCard() {
+
+		
+		Response response = 
+		given().contentType("application/json").body(
+				"{ \"brand\": \"MASTER\", \"customerId\": 1, \"cvc\": \"901\", \"expirationMoth\": 9, \"expirationYear\": 2025, \"holderName\": \"Vinicius\", \"number\": \"5165734389002316\" }")
+				.when().post("/card");
+
+		response.then().statusCode(200).log().all();
+
+		String id = response.jsonPath().getString("id");
+		
+		
+		card_id = id;
+	}
+
 }
--- src/test/java/br/com/southrocklab/resources/CustomerResourceTest.java
+++ src/test/java/br/com/southrocklab/resources/CustomerResourceTest.java
@@ -1,43 +1,213 @@
 package br.com.southrocklab.resources;
 
 import br.com.southrocklab.ApplicationTests;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItems;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class CustomerResourceTest extends ApplicationTests {
-
+public class CustomerResourceTest extends ApplicationTests{
+	
+	String customer_id;
+	String customer_name;
+	String customer_last_name;
+	
+	@BeforeClass
+	public static void urlbase() {
+		RestAssured.port=9090;
+	}
     @Test
     public void deve_salvar_novo_customer_no_sistema() {
-    }
-
+    	Response response = 
+					given()
+						.contentType("application/json")
+						.body("{\"birthDate\" : \"2001-04-04\", \"lastName\" : \"Moreira\", \"name\": \"Vinicius\"}")
+					.when()
+						.post("/customer");
+		
+				response
+					.then()
+						.statusCode(200)
+						.log().all();
+}
+    
     @Test
     public void deve_retornar_status_400_quando_salvar_customer_ja_salvo() {
+    	Response response = 
+				given()
+					.contentType("application/json")
+					.body("{\"birthDate\" : \"2001-04-17\", \"lastName\" : \"Moreira\", \"name\": \"Vinicius\"}")
+				.when()
+					.post("/customer");
+		
+			response.then()
+				.log().all()
+				.body("message", containsString("Customer already exists"))
+				.statusCode(400);
     }
 
     @Test
     public void deve_retornar_status_400_quando_salvar_customer_com_birth_date_maior_que_hoje() {
+    	Response response = 
+				given()
+					.contentType("application/json")
+					.body("{\"birthDate\" : \"2023-04-17\", \"lastName\" : \"Moreira\", \"name\": \"Vinicius\"}")
+				.when()
+					.post("/customer");
+		
+			response.then()
+				.log().all()
+				.body("birthDate", containsString("Birth date must be smaller than today"))
+				.statusCode(400);
     }
 
     @Test
     public void deve_procurar_customer_pelo_name_e_last_name() {
+    	salvandoValores();
+    	System.out.println(customer_last_name+""+customer_name);
+    	Response response = 
+    				given()
+    					.contentType("application/json")
+    					.pathParam("name", customer_name)
+    					.pathParam("last_name", customer_last_name)
+    					.when()
+    					.get("customer/{name}/{last_name}");
+    	
+    			response
+    				.then()	
+    					.log().all()
+    					.statusCode(200);
+    		
+    			response
+    				.then()
+    					.body("name", containsString("Moreira"));
+    			response
+    				.then()
+    					.body("lastName", containsString("Martins"));
+    	
     }
 
     @Test
     public void deve_retornar_status_404_quando_buscar_customer_por_name_e_last_name_inexistente() {
+    	Response response = 
+    				given()
+    					.contentType("application/json")
+    					.when()
+    					.get("customer/nome/sobrenome");
+    	
+    			response
+    				.then()	
+    					.log().all()
+    					.body("message", containsString("Customer not found"))
+    					.body("error", containsString("Not Found"))
+    					.statusCode(404);
     }
 
     @Test
     public void deve_atualizar_o_last_name_de_um_customer() {
+    	salvandoValores();
+    	String valorAtualizado = "valorAtualizado";
+    	Response 
+    		response = 
+				given()
+					.contentType("application/json")
+					.body("{\"birthDate\" : \"2001-04-17\", \"lastName\" : \""+valorAtualizado+"\", \"name\": \"Vinicius\"}")
+					.pathParam("id", customer_id)
+				.when()
+					.put("/customer/{id}");
+    	
+    		response
+    			.then()
+    				.body("lastName", containsString(valorAtualizado));
+	
+			response
+				.then()
+					.statusCode(200)
+					.log().all();
+			
+			
     }
 
     @Test
     public void deve_retornar_status_404_ao_atualizar_um_customer_com_id_nao_salvo_no_sistema() {
+    	String valorAtualizado = "valorAtualizado";
+    	Response 
+    		response = 
+				given()
+					.contentType("application/json")
+					.body("{\"birthDate\" : \"2001-04-17\", \"lastName\" : \""+valorAtualizado+"\", \"name\": \"Vinicius\"}")
+					.pathParam("id", 10)
+				.when()
+					.put("/customer/{id}");
+	
+    	response
+		.then()	
+			.body("message", containsString("Customer not found"))
+			.statusCode(404)
+			.body("error", containsString("Not Found"))
+			.log().all();
     }
 
     @Test
     public void deve_deletar_um_customer_salvo_no_sistema() {
+    	salvandoValores();
+    	String valorAtualizado = "valorAtualizado";
+    	Response 
+    		response = 
+				given()
+					.contentType("application/json")
+					.pathParam("id", customer_id)
+				.when()
+					.delete("/customer/{id}");
+	
+			response
+				.then()
+					.statusCode(204)
+					.log().all();
     }
 
     @Test
     public void deve_retornar_status_404_ao_deletar_um_customer_com_id_nao_salvo_no_sistema() {
+    	String valorAtualizado = "valorAtualizado";
+    	Response 
+    		response = 
+				given()
+					.contentType("application/json")
+				.when()
+					.delete("/customer/10");
+	
+			response
+				.then()
+					.statusCode(404)
+					.body("error", containsString("Not Found"))
+					.log().all();
+			
+    }
+    
+    private void salvandoValores() {
+    	Response response = 
+				given()
+					.contentType("application/json")
+					.body("{\"birthDate\" : \"2001-04-16\", \"lastName\" : \"Martins\", \"name\": \"Moreira\"}")
+				.when()
+				.post("/customer");
+		
+				response.then()
+					.statusCode(200)
+					.log().all();
+				
+			String id = response.jsonPath().getString("id");
+			String name = response.jsonPath().getString("name");
+			String last_name = response.jsonPath().getString("lastName");
+			
+			
+			customer_id = id;
+			customer_last_name = last_name;
+			customer_name = name;
     }
 }
